c61e961594535e1b21974326403d92817ba65216
Molid <delmolid@hotmail.fr>
2024-10-17 09:59:15 +0200

    commit n°5

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+function sayHello (firstname,hour) {
 let message = "Bonjour !"
-let firstname = "Beyonce"
-console.log(firstname);
-message = "Bonjour" + " " + firstname + " " + "!" 
-console.log(message);
+message = "Bonjour" + " " + firstname + " " + "!"
+if (hour >= 18){
+  console.log("Bonsoir" + " " + firstname + " !")  
+}
+else {
+console.log(message)
+}
+}
+sayHello(`Beyonce`, 11);
+sayHello(`Beyonce`, 18);
+sayHello(`Beyonce`, 17);
